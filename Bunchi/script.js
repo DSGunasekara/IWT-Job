44902f22c5a5ce558f6159baabff6ac898a836f2
Dilain Gunasekara <adsgunasekara18@gmail.com>
2019-09-01 11:15:35 +0530

    new repo

--- Bunchi/script.js
+++ Bunchi/script.js
@@ -3,33 +3,33 @@ function validateForm() {
   var lname = document.forms["myForm"]["lname"].value;
   var email = document.forms["myForm"]["mail"].value;
   var subject = document.forms["myForm"]["subject"].value;
-  
+
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  
+
   if (fname == "") {
-    alert("Fname must be filled out");
+    alert("First name must be filled out");
     return false;
   }
-  else if(lname == ""){
-	alert("Lname must be filled out");
+  else if (lname == "") {
+    alert("Last name must be filled out");
     return false;
   }
-  else if(email == ""){
+  else if (email == "") {
     alert("email must be filled out");
     return false;
   }
-  else if(subject == ""){
-	alert("Message feild cannot be empty");
+  else if (subject == "") {
+    alert("Message feild cannot be empty");
     return false;
   }
-  else if(!email.match(mailformat)){
-	alert("email is not valid")
-	return false;
+  else if (!email.match(mailformat)) {
+    alert("email is not valid")
+    return false;
   }
-  
+
 }
 
 
 
-  
-  
+
+
