6c16943c79f88d7c5fd135e3efa89425b59de045
Dilain Gunasekara <adsgunasekara18@gmail.com>
2019-09-01 12:51:11 +0530

    updated

--- Thushan/js.js
+++ Thushan/js.js
@@ -1,55 +1,54 @@
-function loadData()
-{
+function loadData() {
 
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
+	const realFileBtn = document.getElementById("real-file");
+	const customBtn = document.getElementById("custom-button");
+	const customTxt = document.getElementById("custom-text");
 
-customBtn.addEventListener("click",function(){
-    realFileBtn.click();
-});
+	customBtn.addEventListener("click", function () {
+		realFileBtn.click();
+	});
 
-realFileBtn.addEventListener("change",function(){
-    if (realFileBtn.value){
-        customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-    }else{
-        customBtn.innerHTML = "No file chose";
-    }
-});
+	realFileBtn.addEventListener("change", function () {
+		if (realFileBtn.value) {
+			customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+		} else {
+			customBtn.innerHTML = "No file chose";
+		}
+	});
 }
 
-function validateForm(){
-	
-	var email, password, retypepw, phone, ;
-	
+function validateForm() {
+
+	var email, password, retypepw, phone;
+
 	email = document.regForm.email.value;
 	password = document.regForm.passw.value;
-    retypepw = document.regForm.repasswd.value;
-    phone = document.regForm.phoneNo.value;
-	
-	
-	
-	if(password.length < 8){
-		
+	retypepw = document.regForm.repasswd.value;
+	phone = document.regForm.phoneNo.value;
+
+
+
+	if (password.length < 8) {
+
 		alert("Password is too short");
-	return false;
+		return false;
 	}
-	
-	else if(password != retypepw){
-		
+
+	else if (password != retypepw) {
+
 		alert("Passwords are not matching");
-	return false;	
+		return false;
 	}
-    else if(phone.length != 10 || !phone.match(/^[0-9]{10}$/)){
-		
+	else if (phone.length != 10 || !phone.match(/^[0-9]{10}$/)) {
+
 		alert("Give valid phone number");
-	return false;	
+		return false;
 	}
-	
-	else{
-		
+
+	else {
+
 		alert("Register Form submitted successfully");
-	
-	return true;	
+
+		return true;
 	}
 }
