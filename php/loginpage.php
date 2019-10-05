f2d89a1c8a72c1f05933164d5715fc1256cfdc93
Dilain Gunasekara <adsgunasekara18@gmail.com>
2019-10-06 00:44:47 +0530

    login page added

--- php/loginpage.php
+++ php/loginpage.php
@@ -2,9 +2,24 @@
 
 require_once "dbcon.php";
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+session_start();
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    $sql = "SELECT * FROM job_seekers where password = '$password';";
+    $query = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($query);
+    $job_seeker_id = $row['job_seeker_id'];
+    $db_password = $row['password'];
 
+    if($password == $db_password){
+        $_SESSION['email'] = $email;
+        $_SESSION['job_seeker_id'] = $job_seeker_id;
+        header("Location: profile.php");
+    }else {
+        echo "Invalid data!";
+    }
+}
 
 ?>
